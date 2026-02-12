9bc6fd9495f1ff0c69aa77b54eb65fb33eb46e94
dgkf <18220321+dgkf@users.noreply.github.com>
2026-02-12 16:27:57 -0500

    feat: add session() function for quarto report

---
+++ R/session.R
@@ -0,0 +1,21 @@
+#' Collect session information
+#'
+#' Catalogues characteristics of the R session and execution environment.
+#'
+#' @export
+session <- function() {
+  # NOTE: data that is provided as named character vectors are coerced to
+  # lists because the names otherwise get lost when passed as a quarto parameter
+
+  list(
+    time = Sys.time(),
+    r_version = unclass(R.version),
+    session_info = sessionInfo(),
+    platform = .Platform,
+    machine = .Machine,
+    rng_kind = RNGkind(),
+    capabilities = as.list(capabilities()),
+    external_software = as.list(extSoftVersion()),
+    graphics_software = as.list(grSoftVersion())
+  )
+}
